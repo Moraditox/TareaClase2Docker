d78e88efc0161eb5faa9c6a6cd43ede39b8df87c
Moraditox <you@example.hectorgamerfmn@gmail.com>
2024-05-29 19:26:22 +0200

    Modificacion del Main

--- src/main/java/SQL/Main.java
+++ src/main/java/SQL/Main.java
@@ -1,10 +1,5 @@
 package SQL;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 public class Main {
     public static void main(String[] args) {
         ConsultasSQL consultas = new ConsultasSQL();
